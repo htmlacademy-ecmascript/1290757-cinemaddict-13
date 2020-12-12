21b173a1a73063cb9715e247266ac36c5554cd02
Alex Silver <Silwar40k@gmail.com>
2020-12-12 18:53:46 +0300

    8. Большие перемены (часть 1): Добавил возможность повторно инициализировать презентер задачи, добавим метод destroy для удаления компонентов

--- src/presenter/film.js
+++ src/presenter/film.js
@@ -1,6 +1,6 @@
 import FilmView from "../view/film.js";
 import PopupView from "../view/popup.js";
-import {render} from "../utils/render.js";
+import {render, remove, replace} from "../utils/render.js";
 import {RenderPosition} from "../const.js";
 
 export default class Film {
@@ -18,12 +18,34 @@ export default class Film {
   init(film) {
     this._film = film;
 
+    const prevFilmView = this._filmView;
+    const prevPopupView = this._popupView;
+
     this._filmView = new FilmView(film);
     this._popupView = new PopupView(this._film);
 
     this._filmView.setFilmHandler(this._onDetailFilmShow);
 
-    render(this._filmContainer, this._filmView, RenderPosition.BEFORE_END);
+    if (prevFilmView === null || prevPopupView === null) {
+      render(this._filmContainer, this._filmView, RenderPosition.BEFORE_END);
+      return;
+    }
+
+    if (this._filmContainer.contains(prevFilmView.element)) {
+      replace(this._filmView, prevFilmView);
+    }
+
+    if (this._bodyContainer.contains(prevPopupView.element)) {
+      replace(this._popupView, prevPopupView);
+    }
+
+    remove(prevFilmView);
+    remove(prevPopupView);
+  }
+
+  _destroy() {
+    remove(this._filmView);
+    remove(this._popupView);
   }
 
   _popupClose() {
